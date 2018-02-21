214a1f3de7e9b47e2a657716b17f27e235bcade3
afrizaldea <afrizaldea@gmail.com>
2018-02-21 18:15:28 +0700

    version 1.0.2 hafid error

--- toko-material/index.php
+++ toko-material/index.php
@@ -1,12 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-  		<meta name="viewport" content="width=device-width, initial-scale=1">
-  		<meta name="description" content="">
-  		<meta name="author" content="">
-	</head>
-	<body>
-		<h1>INI KODINGAN UTK MATRIAL</h1>
+		
+		<P> akdlkaskdlawkle </p>
 	</body>
 </html>
