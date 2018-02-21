92bbcb69c09db5946a4fde0f380facdfe86bef1c
afrizaldea <afrizaldea@gmail.com>
2018-02-21 18:06:20 +0700

    version: 1.0.0

---
+++ asset/img/small/index.css
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<meta name="description" content="">
+  		<meta name="author" content="">
+	</head>
+	<body>
+		<h1>ANOTERO.ID</h1>
+	</body>
+</html>
