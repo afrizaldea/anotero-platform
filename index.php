26a039059c3d2d09e8fc0e596a51305b9b93156a
GyBear <gegyhamdani@gmail.com>
2018-02-22 07:12:36 +0700

    version 1.0.2 layout-1-index

--- index.php
+++ index.php
@@ -1,13 +1,171 @@
-
 <!DOCTYPE html>
-<html>
-	<head>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-  		<meta name="viewport" content="width=device-width, initial-scale=1">
-  		<meta name="description" content="">
-  		<meta name="author" content="">
-	</head>
-	<body>
-		<h1>ANOTERO.ID</h1>
-	</body>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>ANOTERO - PLATFORM</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="asset/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="asset/css/custom/style.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+		<!-- Navigation -->
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+		  <div class="container">
+			<a class="navbar-brand" href="#">ANOTERO</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+			  <span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+			  <ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+				  <a class="nav-link" href="#">Home
+					<span class="sr-only">(current)</span>
+				  </a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link" href="#">Tentang</a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link" href="#">Layanan</a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link" href="#">Login</a>
+				</li>
+			  </ul>
+			</div>
+		  </div>
+		</nav>
+
+		<!-- Page Content -->
+		<div class="container">
+
+		  <!-- Page Heading -->
+		  <h1 class="my-4"><center>Anotero Platform</center></h1>
+
+		  <hr>
+
+		  <div class="row">
+			<div class="col-lg-4 col-sm-6 portfolio-item">
+			  <div class="card h-100">
+				<a href="#"><img class="card-img-top" src="asset/img/medium/apotek.jpg" alt=""></a>
+				<div class="card-body">
+				  <h4 class="card-title">
+					<a href="#">APOTEK</a>
+				  </h4>
+				  <p class="card-text">Buat Program Apotek mu Disini !</p>
+				</div>
+			  </div>
+			</div>
+			<div class="col-lg-4 col-sm-6 portfolio-item">
+			  <div class="card h-100">
+				<a href="#"><img class="card-img-top" src="asset/img/medium/clothing.jpg" alt=""></a>
+				<div class="card-body">
+				  <h4 class="card-title">
+					<a href="#">CLOTHING</a>
+				  </h4>
+				  <p class="card-text">Buat Program Clothing mu Disini !</p>
+				</div>
+			  </div>
+			</div>
+			<div class="col-lg-4 col-sm-6 portfolio-item">
+			  <div class="card h-100">
+				<a href="#"><img class="card-img-top" src="asset/img/medium/hotel.jpg" alt=""></a>
+				<div class="card-body">
+				  <h4 class="card-title">
+					<a href="#">HOTEL</a>
+				  </h4>
+				  <p class="card-text">Buat Program Hotel mu Disini !</p>
+				</div>
+			  </div>
+			</div>
+			<div class="col-lg-4 col-sm-6 portfolio-item">
+			  <div class="card h-100">
+				<a href="#"><img class="card-img-top" src="asset/img/medium/restaurant.jpg" alt=""></a>
+				<div class="card-body">
+				  <h4 class="card-title">
+					<a href="#">RESTAURANT</a>
+				  </h4>
+				  <p class="card-text">Buat Program Restaurant mu Disini !</p>
+				</div>
+			  </div>
+			</div>
+			<div class="col-lg-4 col-sm-6 portfolio-item">
+			  <div class="card h-100">
+				<a href="#"><img class="card-img-top" src="asset/img/medium/mini-market.jpg" alt=""></a>
+				<div class="card-body">
+				  <h4 class="card-title">
+					<a href="#">MINI-MARKET</a>
+				  </h4>
+				  <p class="card-text">Buat Program Mini-Market mu Disini !</p>
+				</div>
+			  </div>
+			</div>
+			<div class="col-lg-4 col-sm-6 portfolio-item">
+			  <div class="card h-100">
+				<a href="#"><img class="card-img-top" src="asset/img/medium/toko-elektronik.jpg" alt=""></a>
+				<div class="card-body">
+				  <h4 class="card-title">
+					<a href="#">TOKO ELEKTRONIK</a>
+				  </h4>
+				  <p class="card-text">Buat Program Toko Material mu Disini !</p>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		  <!-- /.row -->
+
+      <!-- Pagination -->
+      <ul class="pagination justify-content-center">
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">1</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">2</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">3</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>
+      </ul>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; ANOTERO.ID</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="asset/js/jquery/jquery.min.js"></script>
+    <script src="asset/js/bootstrap/bootstrap.bundle.min.js"></script>
+
+  </body>
+
 </html>
