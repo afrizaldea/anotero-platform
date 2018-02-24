1c527e8e78334952bc9e840e2de29dc620fc945f
afrizaldea <afrizaldea@gmail.com>
2018-02-24 19:39:35 +0700

    version 1.1.14 register modified design

--- user/register.php
+++ user/register.php
@@ -1,116 +1,123 @@
-<?php
-
-/*
-############
-ketika form di load, maka tidak masuk ke model secara langsung karena itu bahaya
-maka di include lah si control user sebagai perantara
-############
-*/
-include "../control/User.php";
-$user = new User();
-$user->register();
-$capca = $user->capca();
-//proses teknikal selesai
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
-	<head>
-
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="description" content="">
-		<meta name="author" content="">
-
-		<title>Registrasi Anotero</title>
-
-		<link href="../asset/css/custom/style-login.css" rel="stylesheet">
-		<link href="../asset/css/custom/switch.css" rel="stylesheet">
-
-	</head>
-
-	<body>
-
-		<ul class="topnav">
-			<div class="kiri">
-  				<li><a href="#about"> ANOTERO </a></li>
-  			</div>
-  			<div class="kanan">
-  				<li class="right"><a href="login.php">Login</a></li>
-  				<li class="right"><a href="../index.php #produk">Layanan</a></li>
-  				<li class="right"><a href="../index.php #about">Tentang</a></li>
-  				<li class="right"><a href="../index.php #home">Home</a></li>
-  			</div>		
-		</ul>
+<head>
+	<title>Login V1</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="../asset/dashboard-libs/image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../asset/dashboard-libs/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../asset/dashboard-libs/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../asset/dashboard-libs/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="../asset/dashboard-libs/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../asset/dashboard-libs/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../asset/dashboard-libs/css/util.css">
+	<link rel="stylesheet" type="text/css" href="../asset/dashboard-libs/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
 	
-		<div class="form_wrapper">
-			<div class="form_container">
-				<div class="title_container">
-					<h2>BUAT AKUN</h2>
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="../asset/dashboard-libs/images/img-01.png" alt="IMG">
 				</div>
-				<div class="center">
-					<form action="" method="POST">
-						<div class="input_field"><span><small><div class="sepan">&nbsp; Nama Perusahaan</div></small></span>
-							<input type="text2" name="nama" required=""/>
-						</div>
-						<div class="input_field"><span><small><div class="sepan">&nbsp; Email</div></small></span>
-							<input type="email2" name="email" required=""/>
-						</div>
-						<div class="input_field"><span><small><div class="sepan">&nbsp; Password</div></small></span>
-							<input type="password2" name="password" required=""/>
-						</div>
-						<div class="input_field"><span><small><div class="sepan">&nbsp; Confrim Password</div></small></small></span>
-							<input type="password2" name="confimpassword" required=""/>
-						</div>
-    					<select>
-    						<option value="0">-----</option>
-      						<option value="1">Mini - Market</option>
-      						<option value="2">Apotek</option>
-      						<option value="3">Hotel</option>
-      						<option value="4">Restaurant</option>
-      						<option value="5">Clothing</option>
-      						<option value="6">Toko Elektronik</option>
-    					</select>
-    					</br>
-						<fieldset>
-			                <legend>Captcha</legend>
-				                <label><?php echo $capca['cap1'] . "+" . $capca['cap2'] ?></label>
-					            <br/>
-					            <input type="text" onkeypress=" return angka(event)" onkeyup="capca(<?php echo $capca['cap3']; ?>)" id="nilai" >
-			            </fieldset>
-			            <br/>
-    					<p> Saya menyetujui untuk membagikan data pendaftaran kepada penyedia layanan Anotero</p>
-						<label class="switch">
-  							<input type="checkbox">
-  							<span class="slider round"></span>
-						</label>
-						</p>
-						<input id="button" disabled type="submit" value="Daftar"/>
-					</form>
-				</div>
+
+				<form class="login100-form validate-form" method="POST" action="">
+					<span class="login100-form-title">
+						Registrasi Sekarang!
+					</span>
+                    
+					<div class="wrap-input100 validate-input" data-validate = "Format email harus valid, contoh: ex@abc.xyz">
+						<input class="input100" type="email" name="email" placeholder="Email">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-user-circle" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Password harus di inputkan">
+						<input class="input100" type="password" name="password" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+                    
+                    <div class="wrap-input100 validate-input" data-validate = "masukan nama perusahaan anda">
+						<input class="input100" type="text" name="nama_perusahaan" placeholder="nama perusahaan anda">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-id-card-o" aria-hidden="true"></i>
+						</span>
+					</div>
+                    
+                    <div class="wrap-input100 validate-input" data-validate = "masukan jenis_usaha anda">
+						<select class="input100" type="text" name="jenis_usaha" placeholder="jenis usaha anda">
+						<span class="focus-input100"></span>
+						<option class="symbol-input100">
+							Data ke 1
+						</option>
+                            <option class="symbol-input100">
+							Data ke 2
+						</option>
+                            <option class="symbol-input100">
+							Data ke 3
+						</option>
+					   </select>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Daftarkan Saya
+						</button>
+					</div>
+
+					<div class="text-center p-t-12">
+						<span class="txt1">
+							Anda Sudah Punya Akun?
+						</span>
+						<a class="txt2" href="#">
+							Masuk disini!
+						</a>
+					</div>
+
+					<div class="text-center p-t-136">
+						<a class="txt2" href="#">
+							Daftar menggunakan Gmail
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
 			</div>
 		</div>
+	</div>
 	
-	</body>
+	
+
+	
+<!--===============================================================================================-->	
+	<script src="../asset/dashboard-libs/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../asset/dashboard-libs/vendor/bootstrap/js/popper.js"></script>
+	<script src="../asset/dashboard-libs/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../asset/dashboard-libs/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../asset/dashboard-libs/vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="../asset/dashboard-libs/js/main.js"></script>
+
+</body>
 </html>
-<script>
-    var hidden = false;
-    function capca(x) {
-	    var y = document.getElementById('nilai').value;
-        if(x==y) {
-            document.getElementById('button').disabled = false;
-        } else {
-            document.getElementById('button').disabled = true;
-        }
-    }
-	function angka(evt){
-	    var charCode = (evt.which) ? evt.which : event.keyCode
-		if(charCode > 31 && (charCode < 48 || charCode > 57))
-		
-		return false;
-		return true;
-	}
-</script>
