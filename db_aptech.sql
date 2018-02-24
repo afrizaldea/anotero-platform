ad6af595cde92a5fbdbf2878c75a83d9042a071c
alsagunadi <alsagunadi@gmail.com>
2018-02-24 13:09:38 +0700

    version 1.1.2 database-change

--- db_aptech.sql
+++ db_aptech.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2018 at 12:54 PM
+-- Generation Time: Feb 24, 2018 at 07:06 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 5.6.32
 
@@ -32,22 +32,35 @@ CREATE TABLE `tb_user` (
   `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `nama_perusahaan` varchar(255) NOT NULL,
-  `jenis_usaha` varchar(255) NOT NULL,
-  `asal_kota` varchar(255) DEFAULT NULL,
-  `status` enum('0','1') NOT NULL,
-  `alamat` varchar(255) DEFAULT NULL,
-  `website` varchar(255) DEFAULT NULL
+  `hak_akses` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_user`
 --
 
-INSERT INTO `tb_user` (`id`, `email`, `password`, `nama_perusahaan`, `jenis_usaha`, `asal_kota`, `status`, `alamat`, `website`) VALUES
-(1, 'dea@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'DEA AF', 'mini-market', '', '0', '', ''),
-(2, 'dea_afrizal@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'afrizal', 'toko-matrial', '', '0', '', ''),
-(5, 'africorp@gmail.com', 'f10e2821bbbea527ea02200352313bc059445190', 'apricorp', 'mini-market', '', '0', '', '');
+INSERT INTO `tb_user` (`id`, `email`, `password`, `hak_akses`) VALUES
+(1, 'dea@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', ''),
+(2, 'dea_afrizal@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', ''),
+(5, 'africorp@gmail.com', 'f10e2821bbbea527ea02200352313bc059445190', ''),
+(6, 'dea@gmail.com', 'f10e2821bbbea527ea02200352313bc059445190', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user_profil`
+--
+
+CREATE TABLE `tb_user_profil` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `nama_perusahaan` varchar(255) NOT NULL,
+  `jenis_usaha` varchar(255) NOT NULL,
+  `asal_kota` enum('0','1') NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `website` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -60,6 +73,12 @@ ALTER TABLE `tb_user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tb_user_profil`
+--
+ALTER TABLE `tb_user_profil`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -67,7 +86,13 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tb_user_profil`
+--
+ALTER TABLE `tb_user_profil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
