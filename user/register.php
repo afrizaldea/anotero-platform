158cb587be97a913086c013c224aa6efb911cf5e
Teday <tedi.suryadi77@gmail.com>
2018-02-25 15:26:43 +0700

    version 1.1.19 Capca dan konfirmasi kata sandi Register

--- user/register.php
+++ user/register.php
@@ -3,7 +3,7 @@
 include "../control/User.php";
 	$user = new User();
 	$user->register();
-
+    $capca = $user->capca();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -58,7 +58,7 @@ include "../control/User.php";
               
         <!--========== END NAVBAR ==========-->
     
-    
+        <!--========== FORM ==========-->
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
@@ -67,13 +67,13 @@ include "../control/User.php";
 					<img src="../asset/img/medium/user-register.png" alt="IMG">
 				</div>
 
-				<form class="login100-form validate-form" method="POST" action="">
+				<form class="login100-form validate-form" method="POST" action="" onsubmit="return validasi_pass(this)">
 					<span class="login100-form-title">
 						Registrasi Sekarang!
 					</span>
                     
 					<div class="wrap-input100 validate-input" data-validate = "Format email harus valid, contoh: ex@abc.xyz">
-						<input class="input100" type="email" name="email" placeholder="Email">
+						<input class="input100" type="email" name="email" placeholder="Email" required>
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-user-circle" aria-hidden="true"></i>
@@ -81,7 +81,7 @@ include "../control/User.php";
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Kata sandi harus di inputkan">
-						<input class="input100" type="password" name="password" placeholder="Kata sandi">
+						<input class="input100" type="password" name="password" placeholder="Kata sandi" required>
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
@@ -89,14 +89,14 @@ include "../control/User.php";
 					</div>
                     
                     <div class="wrap-input100 validate-input" data-validate = "Kata sandi harus di inputkan">
-						<input class="input100" type="password" name="password" placeholder="Konfirmasi Kata sandi">
+						<input class="input100" type="password" name="konfirmasi" placeholder="Konfirmasi Kata sandi" required>
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
 						</span>
 					</div>
                     <div class="wrap-input100 validate-input" data-validate = "Masukan nama perusahaan anda">
-						<input class="input100" type="text" name="nama_perusahaan" placeholder="Nama perusahaan anda">
+						<input class="input100" type="text" name="nama_perusahaan" placeholder="Nama perusahaan anda" required>
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-id-card-o" aria-hidden="true"></i>
@@ -104,7 +104,7 @@ include "../control/User.php";
 					</div>
                     
                     <div class="wrap-input100 validate-input" data-validate = "masukan jenis_usaha anda">
-						<select class="input100" type="text" name="jenis_usaha" placeholder="jenis usaha anda">
+						<select class="input100" type="text" name="jenis_usaha" placeholder="jenis usaha anda" required>
 						<span class="focus-input100"></span>
 						<option class="symbol-input100" value="">
 							Pilih Jenis Usaha
@@ -129,10 +129,17 @@ include "../control/User.php";
 						</option>
 						
 					   </select>
-                    </div>    
+                    </div>
+                    <fieldset>
+			            <legend>Captcha</legend>
+				            <label><?php echo $capca['cap1'] . "+" . $capca['cap2'] ?></label>
+					        <br/>
+					        <input type="text" class="input100" onkeyup="capca(<?php echo $capca['cap3']; ?>)" id="nilai" >
+			        </fieldset>
+			            <br/>					
 					
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
+						<button class="login100-form-btn" id="button" disabled>
 							Daftarkan Saya
 						</button>
 					</div>
@@ -154,6 +161,7 @@ include "../control/User.php";
 					</div>
     
 				</form>
+				<!--========== END FORM ==========-->
 			</div>
 		</div>
 	</div>
@@ -180,3 +188,23 @@ include "../control/User.php";
 <!-- end js script form -->
 </body>
 </html>
+
+<script>
+    var hidden = false;
+    function capca(x) {
+	    var y = document.getElementById('nilai').value;
+        if(x == y) {
+            document.getElementById('button').disabled = false;
+        } else {
+            document.getElementById('button').disabled = true;
+        }
+    }
+	function validasi_pass(form){
+        if (form.password.value != form.konfirmasi.value){
+            alert("Kata sandi dan konfirmasi kata sandi harus sama");
+            form.konfirmasi.focus();
+            return (false);
+        }
+        return (true);
+    }
+</script>
